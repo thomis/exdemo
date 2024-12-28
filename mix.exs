cf8a1dadfe0adf011ed25177a26172ab908ed2e2
thomis <thomas.steiner@ikey.ch>
2024-12-28 12:14:38 +0100

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Exdemo.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.7.17"},
-      {:phoenix_html, "~> 4.1"},
+      {:phoenix_html, "~> 4.2"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.0.0"},
       {:floki, ">= 0.30.0", only: :test},
