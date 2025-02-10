8a0b0c6b11a2b058ea52eb6c4d5a5e32e49856d3
thomis <thomas.steiner@ikey.ch>
2025-02-10 20:52:10 +0100

    Update dependencies

--- config/config.exs
+++ config/config.exs
@@ -38,7 +38,7 @@ config :exdemo, Exdemo.Mailer, adapter: Swoosh.Adapters.Local
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.24.1",
+  version: "0.25.0",
   exdemo: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
